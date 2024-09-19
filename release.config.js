ee5eca4f6308837e4c9dff26761654750e0371af
175022@unsaac.edu.pe <174445@unsaac.edu.pe>
2024-09-19 17:05:12 -0500

    chore: agregar configuración de Semantic Release

--- release.config.js
+++ release.config.js
@@ -1,11 +1,12 @@
+//Semantic Rlease
 module.exports = {
-  branches: ["main"],  // Se ejecutará en la rama main
+  branches: ["main"],  // Solo se lanzará en la rama "main"
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    "@semantic-release/git"
+    "@semantic-release/commit-analyzer",      // Analiza los commits para determinar el tipo de versión
+    "@semantic-release/release-notes-generator", // Genera notas de la versión automáticamente
+    "@semantic-release/changelog",           // Actualiza el changelog
+    "@semantic-release/github",              // Crea un release en GitHub
+    "@semantic-release/git"                  // Actualiza archivos en Git
   ],
 };
 
